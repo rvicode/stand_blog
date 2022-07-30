e68fa023da1c557b70c57b9d209a1d0628289f7c
rvicode <arvinvisi03@gmail.com>
2022-07-30 13:26:12 +0430

    create translate admin

--- blog/models.py
+++ blog/models.py
@@ -3,32 +3,45 @@ from accounts.models import CustomUser
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=40, verbose_name='مقدمه')
 
     def __str__(self):
         return str(self.title or '')
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=40)
-    description = models.TextField()
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    topic = models.ManyToManyField(Category, related_query_name='topic_list')
-    created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=40, verbose_name='مقدمه')
+    description = models.TextField(verbose_name='بدنه')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='نویسنده')
+    topic = models.ManyToManyField(Category, related_query_name='topic_list', verbose_name='موضوع')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ نشر')
+    update = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروز رسانی')
     slug = models.SlugField(default=title)
-    image = models.ImageField(upload_to='media/blog/article')
+    image = models.ImageField(upload_to='media/blog/article', verbose_name='تصویر')
 
     def __str__(self):
         return str(self.title or '')
 
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقاله ها'
+
 
 class Comment(models.Model):
-    username = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comment')
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comment')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='reply', null=True, blank=True)
-    massage = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    username = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comment', verbose_name='نام کاربری')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comment', verbose_name='مقاله')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='reply', null=True, blank=True, verbose_name='پاسخ به کامنت')
+    massage = models.TextField(verbose_name='پیام')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ')
 
     def __str__(self):
         return str(self.massage)
+
+
+    class Meta:
+        verbose_name = 'کامنت'
+        verbose_name_plural = 'کامنت ها'
--- contact_us/models.py
+++ contact_us/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class ContactUs(models.Model):
-    username = models.CharField(max_length=30)
-    email = models.EmailField()
-    number = models.CharField(max_length=13)
-    subject = models.CharField(max_length=40)
-    massage = models.TextField()
+    username = models.CharField(max_length=30, verbose_name='نام کاربری')
+    email = models.EmailField(verbose_name='ایمیل')
+    number = models.CharField(max_length=13, verbose_name='شماره تلفن')
+    subject = models.CharField(max_length=40, verbose_name='موضوع')
+    massage = models.TextField(verbose_name='پیغام')
 
     def __str__(self):
         return f'{self.username} : {self.subject}'
+
+    class Meta:
+        verbose_name = 'ارتباط با ما'
+        verbose_name_plural = 'ارتباطات'
+
