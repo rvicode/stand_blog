51c473ccbf215255e9849814a68eb7aac6457f31
rvicode <arvinvisi03@gmail.com>
2022-07-31 14:45:28 +0430

    set the clas FilterbyTitle

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 from .models import Article, Category, Comment
 
 
+class FilterByTitle(admin.SimpleListFilter):
+    title = 'تکرار مقدمه'
+    parameter_name = 'title'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('django', 'جنگو'),
+            ('python', 'پایتون'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(title__icontains=self.value())
+
+
 @admin.register(Article)
 class HomeAdmin(admin.ModelAdmin):
     list_display = ('title', 'created')
+    list_filter = ('created', FilterByTitle)
     search_fields = ('title', 'description')
 
 
